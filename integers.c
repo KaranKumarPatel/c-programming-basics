f06a19f997f92f39a14f1dfa7debe4d4c299c512
Karan Kumar Patel <karanpatel9e@gmail.com>
2026-09-05 11:22:43 +0530

    Add files via upload

---
+++ integers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int x = 7 , y = 8 ;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    x = x + 7;
+    printf("%d\n",x);
+    y = y + 8;
+    x = x - 7;
+
+
+    printf("%d\n",x);
+    printf("%d\n",y);
+    
+    return 0;
+
+}
