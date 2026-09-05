db59c2962016a43a90c87211cfdb3c0593d9cbed
Karan Kumar Patel <karanpatel9e@gmail.com>
2026-09-05 11:49:12 +0530

    Add files via upload

---
+++ arithmetic_operators_using_Float.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float x , y ;
+    x = 5;
+    y = 2;
+    printf(" Additn of the given numbers is : %f \n", x + y);
+    printf(" Subtractn of the given numbers is : %f \n", x - y);
+    printf(" Multiple of the given numbers is : %f \n", x * y);
+    printf(" Division of the given numbers is : %f \n", x / y);
+    return 0;
+}    
+
---
+++ arithmetic_operators_using_int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x , y ;
+    x = 5;
+    y = 2;
+    printf(" Additn of the given numbers is : %d \n", x + y);
+    printf(" Subtractn of the given numbers is : %d \n", x - y);
+    printf(" Multiple of the given numbers is : %d \n", x * y);
+    printf(" Division of the given numbers is : %d \n", x / y);// IT WILL ONLY GIVE QOUTIENT
+    printf(" Remainder of the given numbers is : %d ", x % y);
+    return 0;
+}    
+
